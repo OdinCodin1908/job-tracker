89fa809c817062e583a5c812b0dca7307caf266c
OdinCodin1908 <andrewwells108@gmail.com>
2026-04-22 13:13:14 +0100

    Update API URL to production

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API = 'http://localhost:5000/api';
+const API = 'https://job-trackerii.onrender.com/api';
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
